ab07462c677abfcf2422573c8d1842100d2147e6
Yuan.ZHANG <yuan.zhang@hmrc.gsi.gov.uk>
2016-08-25 16:25:59 +0100

    validation

--- app/uk/gov/hmrc/SSTTP/controllers/UserController.scala
+++ app/uk/gov/hmrc/SSTTP/controllers/UserController.scala
@@ -12,35 +12,51 @@ import scala.concurrent.Future
   */
 object UserController extends UserController
 
+case class userInput(Reference:String, Rate:Int, Liabilities:Int, IniPayment:Int, IniDate:String,
+                     StartDate:String, EndDate:String, Frequency:String)
+
 class UserController extends FrontendController with Controller {
   // a form contents user input information
 
   val userInputForm = Form(
-    tuple(
+    mapping(
       "Reference" -> nonEmptyText,
-      "Interest Rate" -> nonEmptyText,
-      "Liabilities" -> nonEmptyText,
-      "Initial Payment" -> nonEmptyText,
+      "Interest Rate" -> number,
+      "Liabilities" -> number,
+      "Initial Payment" -> number,
       "Initial Payment Date" -> nonEmptyText,
       "Start Date" -> nonEmptyText,
       "End Date" -> nonEmptyText,
       "Payment Frequency" -> nonEmptyText
     )
+    (userInput.apply)(userInput.unapply)
   )
-  def check = Action.async{
+  def check = Action{
     implicit request =>
       val input = userInputForm.bindFromRequest()
+      userInputForm.bindFromRequest().fold(
+        success = {
+          Calculate =>
+          val Reference = input.data("Reference")
+          val InterestRate = input.data("Interest Rate")
+          val Liabilities = input.data("Liabilities")
+          val InitialPayment = input.data("Initial Payment")
+          val InitialPaymentDate = input.data("Initial Payment Date")
+          val StartDate = input.data("Start Date")
+          val EndDate = input.data("End Date")
+          val PaymentFrequency = input.data("Payment Frequency")
+          Redirect(routes.HelloWorld.helloWorld())
+        },
+        hasErrors = {
+          formWithErrors =>
+            print("has errr")
+            BadRequest(uk.gov.hmrc.SSTTP.helloworld.html.hello_world(formWithErrors))
+        }
+      )
 
-        val Reference = input.data("Reference")
-        val InterestRate = input.data("Interest Rate")
-        val Liabilities = input.data("Liabilities")
-        val InitialPayment = input.data("Initial Payment")
-        val InitialPaymentDate = input.data("Initial Payment Date")
-        val StartDate = input.data("Start Date")
-        val EndDate = input.data("End Date")
-        val PaymentFrequency = input.data("Payment Frequency")
 
 
-      Future.successful(Redirect(routes.HelloWorld.helloWorld()))
+
+
   }
 }
