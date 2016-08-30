789ed67b688c90d33ebb103bfb9e7934c512b7a1
Yuan.ZHANG <yuan.zhang@hmrc.gsi.gov.uk>
2016-08-30 17:35:08 +0100

    connector

--- app/uk/gov/hmrc/SSTTP/connectora/helloWorldConnector.scala
+++ app/uk/gov/hmrc/SSTTP/connectora/helloWorldConnector.scala
@@ -1,10 +1,33 @@
 package uk.gov.hmrc.SSTTP.connectora
 
+
+import play.api.libs.json.{JsValue, Json}
 import uk.gov.hmrc.play.config.ServicesConfig
+import uk.gov.hmrc.play.http._
 
 /**
   * Created by yuan on 30/08/16.
   */
-object helloWorldConnector extends ServicesConfig{
+object helloWorldConnector extends helloWorldConnector with ServicesConfig{
   lazy val hello = baseUrl("hello-world")
+}
+
+trait helloWorldConnector{
+  val businessRegUrl: String
+  val http: HttpGet with HttpPost
+
+  def submitDetails(userInformation : BetaUserInformationSubmit)(implicit hc: HeaderCarrier) : Future[BetaUserInformationSubmit] = {
+    val userJson = Json.toJson[BetaUserInformationSubmit](userInformation)
+    http.POST[JsValue, BetaUserInformationSubmit](s"$businessRegUrl/business-registration/beta-sign-up", userJson)
+  }
+
+  def getUserDetailsForTests(queryParameter : String)
+                            (implicit hc : HeaderCarrier, rds : HttpReads[BetaUserInformationSubmit])
+  : Future[Option[BetaUserInformationSubmit]] = {
+    http.GET[Option[BetaUserInformationSubmit]](s"$businessRegUrl/business-registration/test-only/get-beta-user/$queryParameter")
+  }
+
+  def clearUsersForTests()(implicit hc : HeaderCarrier) : Future[Option[Response]] = {
+    http.GET[Option[Response]](s"$businessRegUrl/business-registration/test-only/clear-beta-users")
+  }
 }
--- app/uk/gov/hmrc/SSTTP/controllers/UserController.scala
+++ app/uk/gov/hmrc/SSTTP/controllers/UserController.scala
@@ -1,5 +1,4 @@
 package uk.gov.hmrc.SSTTP.controllers
-//package hmrc.SSTTP.app.uk.gov.hmrc.SSTTP.controllers
 
 import play.api.data.Form
 import play.api.data.Forms._
@@ -8,91 +7,113 @@ import play.api.mvc._
 import org.joda.time.Days
 import org.joda.time.LocalDate
 import uk.gov.hmrc.SSTTP.connectora.helloWorldConnector
-
+import uk.gov.hmrc.play.http._
+import org.joda.time.DateTime
+import play.api.libs.json.Json
+import uk.gov.hmrc.time.DateTimeUtils
 
 /**
   * Created by yuan on 25/08/16.
   */
-object UserController extends UserController
+//object UserController extends UserController
 
-case class userInput(Reference:String, Rate:Int, Liabilities:Int, IniPayment:Int, IniDate:String,
+case class userInput(Reference:String, Rate:String, Liabilities:String, IniPayment:String, IniDate:String,
                      StartDate:String, EndDate:String, Frequency:String)
 
-case class Results(Reference:String, Rate:String, Liabilities:String, IniPayment:String, IniDate:String,
-                     StartDate:String, EndDate:String, Frequency:String, Answer:String)
+case class UserInformationSubmit(Reference:String, Rate:String, Liabilities:String, IniPayment:String, IniDate:String,
+                     StartDate:String, EndDate:String, Frequency:String, submissionTime : DateTime)
 
-class UserController extends FrontendController with Controller{
+object UserController extends FrontendController with Controller{
+
+  implicit val format = Json.format[userInput]
   // a form contents user input information
 
-  val userInputForm = Form(
-    mapping(
-      "Reference" -> nonEmptyText,
-      "Interest Rate" -> number,
-      "Liabilities" -> number,
-      "Initial Payment" -> number,
-      "Initial Payment Date" -> nonEmptyText,
-      "Start Date" -> nonEmptyText,
-      "End Date" -> nonEmptyText,
-      "Payment Frequency" -> nonEmptyText
-    )
-    (userInput.apply)(userInput.unapply)
-  )
-
-  var results:Set[Results] = Set.empty
-
-  def check = Action{
-    implicit request =>
-      val input = userInputForm.bindFromRequest()
-      userInputForm.bindFromRequest().fold(
-
-        success = {
-          Calculate =>
-//            val session:Session = request.session +
-//              ("Reference" -> input.data("Reference"))+
-//              ("InterestRate" ->  input.data("Interest Rate")) +
-//              ("Liabilities" ->  input.data("Liabilities")) +
-//              ("InitialPayment" ->  input.data("Initial Payment")) +
-//              ("InitialPaymentDate" ->  input.data("Initial Payment Date")) +
-//              (" StartDate" ->  input.data("Start Date")) +
-//              ("EndDate" ->  input.data("End Date"))+
-//              ("PaymentFrequency" ->  input.data("Payment Frequency"))
-              val Reference = input.data("Reference")
-              val InterestRate = input.data("Interest Rate")
-              val Liabilities = input.data("Liabilities")
-              val InitialPayment = input.data("Initial Payment")
-              val InitialPaymentDate = input.data("Initial Payment Date")
-              val StartDate = input.data("Start Date")
-              val EndDate = input.data("End Date")
-              val PaymentFrequency = input.data("Payment Frequency")
-
-            val session:Session = request.session +
-              ("Reference" -> Reference)+
-              ("InterestRate" -> InterestRate) +
-              ("Liabilities" -> Liabilities) +
-              ("InitialPayment" -> InitialPayment) +
-              ("InitialPaymentDate" -> InitialPaymentDate) +
-              (" StartDate" -> StartDate) +
-              ("EndDate" -> EndDate)+
-              ("PaymentFrequency" -> PaymentFrequency)
-
-//              val Answer = "£"+BigDecimal(getResult(Liabilities.toDouble, InterestRate.toDouble, getDFDays(StartDate, EndDate))).setScale(2,BigDecimal.RoundingMode.HALF_UP).toDouble
-//
-//              def resultSubmit = new Results(Reference, InterestRate, Liabilities, InitialPayment, InitialPaymentDate,
-//              StartDate, EndDate, PaymentFrequency, Answer)
-//
-//              results += resultSubmit
-
-            
-
-          val helloWorld = helloWorldConnector
-          Redirect(helloWorld.hello+"/SSTTP/hello-world").withSession(session)
-        },
-        hasErrors = {
-          formWithErrors =>
-            BadRequest(uk.gov.hmrc.SSTTP.helloworld.html.hello_world(formWithErrors))
-        }
-      )
+  implicit def userDetailsCaptureToSubmit(details: userInput) : UserInformationSubmit = {
+    val forSub = new UserInformationSubmit(
+      details.Reference,
+      details.Rate,
+      details.Liabilities,
+      details.IniPayment,
+      details.IniDate,
+      details.StartDate,
+      details.EndDate,
+      details.Frequency,
+      DateTimeUtils.now)
+    forSub
   }
+
+
+  object UserInformationSubmit {
+    implicit val format = Json.format[UserInformationSubmit]
+  }
+
+//  val userInputForm = Form(
+//    mapping(
+//      "Reference" -> nonEmptyText,
+//      "Interest Rate" -> number,
+//      "Liabilities" -> number,
+//      "Initial Payment" -> number,
+//      "Initial Payment Date" -> nonEmptyText,
+//      "Start Date" -> nonEmptyText,
+//      "End Date" -> nonEmptyText,
+//      "Payment Frequency" -> nonEmptyText
+//    )
+//    (userInput.apply)(userInput.unapply)
+//  )
+//
+//  var results:Set[Results] = Set.empty
+//
+//  def check = Action{
+//    implicit request =>
+//      val input = userInputForm.bindFromRequest()
+//      userInputForm.bindFromRequest().fold(
+//
+//        success = {
+//          Calculate =>
+////            val session:Session = request.session +
+////              ("Reference" -> input.data("Reference"))+
+////              ("InterestRate" ->  input.data("Interest Rate")) +
+////              ("Liabilities" ->  input.data("Liabilities")) +
+////              ("InitialPayment" ->  input.data("Initial Payment")) +
+////              ("InitialPaymentDate" ->  input.data("Initial Payment Date")) +
+////              (" StartDate" ->  input.data("Start Date")) +
+////              ("EndDate" ->  input.data("End Date"))+
+////              ("PaymentFrequency" ->  input.data("Payment Frequency"))
+//              val Reference = input.data("Reference")
+//              val InterestRate = input.data("Interest Rate")
+//              val Liabilities = input.data("Liabilities")
+//              val InitialPayment = input.data("Initial Payment")
+//              val InitialPaymentDate = input.data("Initial Payment Date")
+//              val StartDate = input.data("Start Date")
+//              val EndDate = input.data("End Date")
+//              val PaymentFrequency = input.data("Payment Frequency")
+//
+//            val session:Session = request.session +
+//              ("Reference" -> Reference) +
+//              ("InterestRate" -> InterestRate) +
+//              ("Liabilities" -> Liabilities) +
+//              ("InitialPayment" -> InitialPayment) +
+//              ("InitialPaymentDate" -> InitialPaymentDate) +
+//              (" StartDate" -> StartDate) +
+//              ("EndDate" -> EndDate)+
+//              ("PaymentFrequency" -> PaymentFrequency)
+//
+////              val Answer = "£"+BigDecimal(getResult(Liabilities.toDouble, InterestRate.toDouble, getDFDays(StartDate, EndDate))).setScale(2,BigDecimal.RoundingMode.HALF_UP).toDouble
+////
+////              def resultSubmit = new Results(Reference, InterestRate, Liabilities, InitialPayment, InitialPaymentDate,
+////              StartDate, EndDate, PaymentFrequency, Answer)
+////
+////              results += resultSubmit
+//
+//          val helloWorld = helloWorldConnector
+//          Redirect(helloWorld.hello+"/SSTTP/hello-world").withSession(session)
+//        },
+//        hasErrors = {
+//          formWithErrors =>
+//            BadRequest(uk.gov.hmrc.SSTTP.helloworld.html.hello_world(formWithErrors))
+//        }
+//      )
+//  }
 
   def getDFDays(StartDate:String, EndDate:String): Int = {
     Days.daysBetween(new LocalDate(StartDate),new LocalDate(EndDate) ).getDays()
